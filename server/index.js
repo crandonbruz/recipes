b0f62d0ea111ce7cabb4ef2896324505a6f12c2a
Brandon <bcruz12127@gmail.com>
2024-09-06 01:36:31 -0400

    editing cors to hopefully accept my requests

--- server/index.js
+++ server/index.js
@@ -13,8 +13,10 @@ const corsOptions = {
   origin: "https://recipies-client-git-main-crandonbruzs-projects.vercel.app",
   allowedHeaders: ["Authorization", "Content-Type"],
   credentials: true,
+  methods: "GET, POST, PUT, DELETE",
 };
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
