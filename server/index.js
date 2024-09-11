858decfc725e36d5066240283b6722a37cecad37
Brandon <bcruz12127@gmail.com>
2024-09-11 12:36:40 -0400

    fixing cors

--- server/index.js
+++ server/index.js
@@ -15,8 +15,8 @@ const corsOptions = {
   credentials: true,
   methods: "GET, POST, PUT, DELETE",
 };
-app.use(cors(corsOptions));
 app.options("*", cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
